ac1652a8ef94223691d6dd85835231c08c3c81d1
Brian Call <callbrian@gmail.com>
2016-03-24 17:01:10 -0700

    added manifest to move files for munki ssl into place

---
+++ manifests/munki_ssl.pp
@@ -0,0 +1,35 @@
+class mac_admin::munki_ssl {
+  if $::operatingsystem != 'Darwin' {
+      fail('The munki_ssl module is only supported on Darwin/OS X')
+  }
+  
+  file { ['/Library/Managed Installs', '/Library/Managed Installs/certs/' ]:
+    ensure => directory,
+    owner  => 'root',
+    group  => 'wheel',
+  }
+
+  file { '/Library/Managed Installs/certs/ca.pem':
+    mode    => '0640',
+    owner   => root,
+    group   => wheel,
+    source  => "${$settings::vardir}/ssl/certs/ca.pem",
+    require =>  File['/Library/Managed Installs/certs/'],
+  }
+ 
+  file { '/Library/Managed Installs/certs/clientcert.pem':
+    mode    => '0640',
+    owner   => root,
+    group   => wheel,
+    source  => "${$settings::vardir}/ssl/certs/${clientcert}.pem",
+    require =>  File['/Library/Managed Installs/certs/'],
+  }
+ 
+  file { '/Library/Managed Installs/certs/clientkey.pem':
+    mode    => '0640',
+    owner   => root,
+    group   => wheel,
+    source  => "${$settings::vardir}/ssl/private_keys/${clientcert}.pem",
+    require =>  File['/Library/Managed Installs/certs/'],
+  }
+}
