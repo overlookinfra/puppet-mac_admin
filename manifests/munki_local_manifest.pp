1268268ffe63bb36ae297edcf45350d09115a846
Brian Call <callbrian@gmail.com>
2016-05-17 12:31:27 -0700

    added munki_local_manifest manifest

---
+++ manifests/munki_local_manifest.pp
@@ -0,0 +1,36 @@
+class mac_admin::munki_local_manifest(
+    $munki_catalogs = $mac_admin::params::munki_catalogs,
+    $munki_managed_installs = $mac_admin::params::munki_managed_installs,
+    $munki_managed_uninstalls = $mac_admin::params::munki_managed_uninstalls,
+    $munki_included_manifests = $mac_admin::params::munki_included_manifests,
+    $munki_managed_updates = $mac_admin::params::muki_managed_updates,
+    $munki_optional_installs = $mac_admin::params::munki_optional_installs,
+    ) inherits mac_admin::params {
+
+  if $::operatingsystem != 'Darwin' {
+      fail('The mac_admin module is only supported on Darwin/OS X')
+  } 
+
+  file { '/Library/Managed Installs/manifests/':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'admin',
+    mode   => '0755',
+  }
+
+  file { "/Library/Managed Installs/manifests/${clientcert}.plist":
+    ensure  => 'file',
+    owner   => 'root',
+    group   => 'admin',
+    mode    => '0644',
+    content => template('mac_admin/com.puppet.it.munki_local_manifest.erb'),
+  }
+
+  property_list_key { 'catalogs':
+    ensure     => 'present',
+    path       => "/Library/Managed Installs/manifests/${clientcert}.plist",
+    key        => 'catalogs',
+    value      => ['software1', 'software2'],
+    value_type => 'array',
+  }  
+}
