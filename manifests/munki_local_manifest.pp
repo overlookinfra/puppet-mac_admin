6361068be08bce41444c9151aef2711e67f56304
Brian Call <callbrian@gmail.com>
2016-05-19 14:05:34 -0700

    added fixed template, removed plist provider stuff

--- manifests/munki_local_manifest.pp
+++ manifests/munki_local_manifest.pp
@@ -11,7 +11,7 @@ class mac_admin::munki_local_manifest(
       fail('The mac_admin module is only supported on Darwin/OS X')
   } 
 
-  file { '/Library/Managed Installs/local_manifests/':
+  file { '/Library/Managed Installs/manifests/':
     ensure => 'directory',
     owner  => 'root',
     group  => 'admin',
@@ -24,63 +24,6 @@ class mac_admin::munki_local_manifest(
     group   => 'admin',
     mode    => '0644',
     content => template('mac_admin/com.puppet.it.munki_local_manifest.erb'),
-    require => File['/Library/Managed Installs/local_manifests/'],
+    require => File['/Library/Managed Installs/manifests/'],
   }
-
-  # Build the local Munki manifest
-/*
-  property_list_key { 'catalogs':
-    ensure     => 'present',
-    path       => "/Library/Managed Installs/manifests/${clientcert}.plist",
-    key        => 'catalogs',
-    value      => $munki_catalogs,
-    value_type => 'array',
-    require    => File["/Library/Managed Installs/manifests/${clientcert}.plist"],
-  }  
-
-  property_list_key { 'managed_installs':
-    ensure     => 'present',
-    path       => "/Library/Managed Installs/manifests/${clientcert}.plist",
-    key        => 'managed_installs',
-    value      => $munki_managed_installs,
-    value_type => 'array',
-    require    => File["/Library/Managed Installs/manifests/${clientcert}.plist"],
-  }
-
-  property_list_key { 'managed_uninstalls':
-    ensure     => 'present',
-    path       => "/Library/Managed Installs/manifests/${clientcert}.plist",
-    key        => 'managed_uninstalls',
-    value      => $munki_managed_uninstalls,
-    value_type => 'array',
-    require    => File["/Library/Managed Installs/manifests/${clientcert}.plist"],
-  }
-
-  property_list_key { 'included_manifests':
-    ensure     => 'present',
-    path       => "/Library/Managed Installs/manifests/${clientcert}.plist",
-    key        => 'included_manifests',
-    value      => $munki_included_manifests,
-    value_type => 'array',
-    require    => File["/Library/Managed Installs/manifests/${clientcert}.plist"],
-  }
-
-  property_list_key { 'managed_updates':
-    ensure     => 'present',
-    path       => "/Library/Managed Installs/manifests/${clientcert}.plist",
-    key        => 'managed_updates',
-    value      => $munki_managed_updates,
-    value_type => 'array',
-    require    => File["/Library/Managed Installs/manifests/${clientcert}.plist"],
-  }
-
-  property_list_key { 'optional_installs':
-    ensure     => 'present',
-    path       => "/Library/Managed Installs/manifests/${clientcert}.plist",
-    key        => 'optional_installs',
-    value      => $munki_optional_installs,
-    value_type => 'array',
-    require    => File["/Library/Managed Installs/manifests/${clientcert}.plist"],
-  }
-*/
 }
